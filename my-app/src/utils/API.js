10b260018d82e3c67818e0fa47b5f3332f710493
Nora <honorahegarty23@gmail.com>
2020-03-21 22:01:44 -0700

    updates

--- my-app/src/utils/API.js
+++ my-app/src/utils/API.js
@@ -1,1 +1,25 @@
 import axios from "axios";
+
+export default {
+  getBook: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  },
+
+  // Delete book with the given id //
+
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id).then(result => result.data);
+  },
+
+  // Save  book to the database //
+
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData).then(result => result.data);
+  },
+
+  // Get  saved books from the database // 
+
+  savedBooks: function () {
+    return axios.get("/api/books").then(result => result.data);
+  }
+};
